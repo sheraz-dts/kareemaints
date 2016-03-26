2ff61a48d338439b66de8d61d4007b5f2523f9c4
Muhammad Sheraz <msherazjaved@gmail.com>
2016-03-26 18:04:26 +0500

    -reset changes

--- wp-content/plugins/wp-tour-managment/settings.php
+++ wp-content/plugins/wp-tour-managment/settings.php
@@ -2,7 +2,7 @@
 
 require_once WPTM_PLUGIN_DIR . '/includes/functions.php';
 
-// Checking if logged in user is admin
+// Checking if user is Admin
 if ( is_admin() ) {
 	require_once WPTM_PLUGIN_DIR . '/admin/admin.php';
 } else {
