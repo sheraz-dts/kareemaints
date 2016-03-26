7a59e54d951922c9ace43416e59d2394772a1041
Muhammad Sheraz <msherazjaved@gmail.com>
2016-03-26 16:33:09 +0500

    - added a comment in settings.php

--- wp-content/plugins/wp-tour-managment/settings.php
+++ wp-content/plugins/wp-tour-managment/settings.php
@@ -2,6 +2,7 @@
 
 require_once WPTM_PLUGIN_DIR . '/includes/functions.php';
 
+// Checking if logged in user is admin
 if ( is_admin() ) {
 	require_once WPTM_PLUGIN_DIR . '/admin/admin.php';
 } else {
