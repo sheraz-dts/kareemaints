e7f84198f9f5021a5f259b8764042f7797d1718f
Fareed <fkfareed337@gmail.com>
2016-04-06 02:58:50 +0500

    - Booking Template, trip summary screen added
    - Some style appliyed

---
+++ wp-content/themes/kareemaint/booking-template.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Booking Template
+*/
+
+// Please eik ya do tours add karain....ok
+?>
+
+<?php get_header(); ?>
+	
+	<?php do_action ( 'portlet_tour_client_form' )?>
+    
+<?php get_footer(); ?>
